51cd0651ef69084eed849b1584511125c205e59b
Flynn <fjm1717@ic.ac.uk>
2020-11-19 14:06:38 +0000

    workspace commit

---
+++ signature_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/spyros/signature_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/spyros/ws_moveit/devel;/home/spyros/tortoisebot_ws/devel;/home/spyros/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/spyros/ws_moveit/devel;/home/spyros/tortoisebot_ws/devel;/home/spyros/catkin_ws/devel;/opt/ros/melodic' != '' else []
